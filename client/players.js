8605d4264d28c2ec88b20899f646b226a13c2c8a
jorgeer <jfemail3@gmail.com>
2015-03-02 23:25:01 +0100

    select players

--- client/players.js
+++ client/players.js
@@ -1,7 +1,27 @@
-
+Session.set("playerSelected", false);
 
 Template.players.helpers({
     players: function () {
         return Meteor.users.find({ location: Meteor.user().location });
+    },
+
+    selectedPlayer: function () {
+        return Session.get("playerSelected");
     }
 });
+
+Template.players.events({
+    "click .player": function (e) {
+        var playerId = e.currentTarget.getAttribute("playerid");
+
+        Session.set("playerSelected", playerId);
+    },
+
+    "click [action]": function (e) {
+        var action = e.currentTarget.getAttribute("action");
+
+        switch (action) {
+            case "cancel": Session.set("playerSelected", false); break;
+        }
+    }
+})
